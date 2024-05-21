e38316c50d56e1bc3de3e6de36de5640633047d4
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-20 20:43:31 -0400

    refactor: Update PhotoListItem component structure and class names for applied styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
-
   return (
     <article key={props.data.key}>
-      <div className="photoItem">
-        <img src={props.data.imageSource} alt="image" />
-        <img src={props.data.profile} alt="" />
-        <h2>{props.data.username}</h2>
-        <h3>{props.data.location.city}, {props.data.location.country}</h3>
+      <div className="photo-list__item">
+        <img
+          className="photo-list__image"
+          src={props.data.imageSource}
+          alt="image"
+        />
+        <img
+          className="photo-list__user-profile"
+          src={props.data.profile}
+          alt=""
+        />
+        <div className="photo-list__user-details">
+          <h2 className="photo-list__user-info">{props.data.username}</h2>
+          <h3 className="photo-list__user-location">
+            {props.data.location.city}, {props.data.location.country}
+          </h3>
+        </div>
       </div>
     </article>
   );

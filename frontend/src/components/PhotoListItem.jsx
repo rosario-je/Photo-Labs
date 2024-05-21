66c57b514b2bd0b6aafbcb30ff765146462393cb
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-20 22:38:46 -0400

    refactor: remove reduntant logic to display three items.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,48 +2,30 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
-  
-  const item = function () {
-    return (
-      <article key={props.data.key}>
-        <div className="photo-list__item">
+    
+  return (
+    <article key={props.data.key}>
+      <div className="photo-list__item">
+        <img
+          className="photo-list__image"
+          src={props.data.imageSource}
+          alt="image"
+        />
+        <div className="photo-list__user-details">
           <img
-            className="photo-list__image"
-            src={props.data.imageSource}
-            alt="image"
+            className="photo-list__user-profile"
+            src={props.data.profile}
+            alt=""
           />
-          <div className="photo-list__user-details">
-            <img
-              className="photo-list__user-profile"
-              src={props.data.profile}
-              alt=""
-            />
-            <div className="photo-list__user-info">
-              <h2>{props.data.username}</h2>
-              <h3 className="photo-list__user-location">
-                {props.data.location.city}, {props.data.location.country}
-              </h3>
-            </div>
+          <div className="photo-list__user-info">
+            <h2>{props.data.username}</h2>
+            <h3 className="photo-list__user-location">
+              {props.data.location.city}, {props.data.location.country}
+            </h3>
           </div>
         </div>
-      </article>
-    );
-  }
-
-  const displayThree = function () {
-    return (
-      <div className="photo-list">
-        {item()}
-        {item()}
-        {item()}
       </div>
-    );
-  }
-
-  return (
-    <section className="photo-list">
-      {displayThree()}
-    </section>
+    </article>
   );
 };
 

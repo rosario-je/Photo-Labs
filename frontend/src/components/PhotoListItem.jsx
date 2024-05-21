610c2fa7aa781fc46b154faa580a554f88761c5f
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-20 21:18:57 -0400

    feat: Add logic to display three individual photos.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,27 +2,48 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
-  return (
-    <article key={props.data.key}>
-      <div className="photo-list__item">
-        <img
-          className="photo-list__image"
-          src={props.data.imageSource}
-          alt="image"
-        />
-        <img
-          className="photo-list__user-profile"
-          src={props.data.profile}
-          alt=""
-        />
-        <div className="photo-list__user-details">
-          <h2 className="photo-list__user-info">{props.data.username}</h2>
-          <h3 className="photo-list__user-location">
-            {props.data.location.city}, {props.data.location.country}
-          </h3>
+  
+  const item = function () {
+    return (
+      <article key={props.data.key}>
+        <div className="photo-list__item">
+          <img
+            className="photo-list__image"
+            src={props.data.imageSource}
+            alt="image"
+          />
+          <div className="photo-list__user-details">
+            <img
+              className="photo-list__user-profile"
+              src={props.data.profile}
+              alt=""
+            />
+            <div className="photo-list__user-info">
+              <h2>{props.data.username}</h2>
+              <h3 className="photo-list__user-location">
+                {props.data.location.city}, {props.data.location.country}
+              </h3>
+            </div>
+          </div>
         </div>
+      </article>
+    );
+  }
+
+  const displayThree = function () {
+    return (
+      <div className="photo-list">
+        {item()}
+        {item()}
+        {item()}
       </div>
-    </article>
+    );
+  }
+
+  return (
+    <section className="photo-list">
+      {displayThree()}
+    </section>
   );
 };
 

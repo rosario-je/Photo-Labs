2870a963f79431233e4173dc67d54c7d185e20ee
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-21 01:20:33 -0400

    refactor: Update PhotoFavButton component to accept props for onClick, selected, and displayAlert

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,12 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+      <div onClick={props.onClick} className="photo-list__fav-icon-svg">
+        <FavIcon selected={props.selected} displayAlert={props.displayAlert}/>
       </div>
     </div>
   );

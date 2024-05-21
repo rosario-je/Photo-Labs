00e14b1a3f25d02ff594b5ade1991f3e7c40202b
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-20 20:02:29 -0400

    Refactor: move data from PhotoListItem.jsx to App.jsx to be passed down as a prop.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
+import PhotoListItem from "./components/PhotoListItem";
+import "./App.scss";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const sampleDataForPhotoListItem = {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  };
+
+  const {
+    id,
+    location: { city, country },
+    imageSource,
+    username,
+    profile,
+  } = sampleDataForPhotoListItem;
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem
+        key={id}
+        city={city}
+        country={country}
+        imageSource={imageSource}
+        username={username}
+        profile={profile}
+      />
     </div>
   );
 };

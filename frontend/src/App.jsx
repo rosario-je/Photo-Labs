2f198be0b7f62e76123f285b94fd841878ce3dd4
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-21 11:57:51 -0400

    refactor: Update App component to use PhotoList instead of PhotoListItem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,20 @@
 import React from "react";
 
-import PhotoListItem from "./components/PhotoListItem";
+import PhotoList from "components/PhotoList";
 import "./App.scss";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const sampleDataForPhotoListItem = {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  };
+  
 
-  const photos = [...Array(3)];
-  const photoListItems = photos.map((photo, i) => (
-    <PhotoListItem key={i} data={sampleDataForPhotoListItem} />
-  ));
+  // const photos = [...Array(3)];
+  // const photoListItems = photos.map((photo, i) => (
+  //   <PhotoListItem key={i} data={sampleDataForPhotoListItem} />
+  // ));
 
   return (
     <div className="App">
-      {photoListItems}
+      <PhotoList />
     </div>
   );
 };

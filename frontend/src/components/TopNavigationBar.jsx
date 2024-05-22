d6e0c3293f40252de0cf21cbae7744d1b35efe8f
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-21 23:35:21 -0400

    feat: Add TopicList and PhotoFavButton components to TopNavigationBar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import TopicList from "components/TopicList";
 import '../styles/TopNavigationBar.scss'
+import PhotoFavButton from './PhotoFavButton';
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList />
+      <span><PhotoFavButton /></span>
     </div>
   )
 }

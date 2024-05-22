00fb360a2682bf7acf45f7965a2aba2e712d46f4
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-22 12:35:28 -0400

    refactor: Update TopNavigationBar component to use props for topics data and fix formatting

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import TopicList from "components/TopicList";
-import '../styles/TopNavigationBar.scss'
-import FavBadge from './FavBadge';
+import React from "react";
 
-const TopNavigation = () => {
+import TopicList from "components/TopicList";
+import FavBadge from "./FavBadge";
+
+import "../styles/TopNavigationBar.scss";
+
+const TopNavigation = (props) => {
+  const { topics } = props;
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList />
-      <FavBadge/>
+      <TopicList topics={topics} />
+      <FavBadge />
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;

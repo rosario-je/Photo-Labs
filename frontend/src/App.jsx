a7ec1e82e41650e3cb7874abaa4b316bfca909d3
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-22 12:34:24 -0400

    refactor: Update App component to pass photos and topics data to HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
+import HomeRoute from "routes/HomeRoute";
+import photos from "mocks/photos";
+import topics from "mocks/topics";
+
 import "./App.scss";
 
-import HomeRoute from "routes/HomeRoute";
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   
 
   return (
     <div className="App">
-        <HomeRoute />
+        <HomeRoute photos={photos} topics={topics}/>
     </div>
   );
 };

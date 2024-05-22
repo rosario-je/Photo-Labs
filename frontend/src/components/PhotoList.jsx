b0114770ae69f67c8a5c8cdbcd623b0aead6573c
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-22 12:35:12 -0400

    refactor: Update PhotoList component to use props for photos data

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
-import photos from "mocks/photos";
 
+import "../styles/PhotoList.scss";
 
-const PhotoList = () => {
-  const allPhotos = photos.map((photo) => 
+const PhotoList = (props) => {
+  const { photos } = props;
+  const allPhotos = photos.map((photo) => (
     <PhotoListItem key={photo.id} data={photo} />
-  );
+  ));
   return <ul className="photo-list">{allPhotos}</ul>;
 };
 

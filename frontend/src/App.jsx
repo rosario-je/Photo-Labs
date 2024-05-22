f8e21e520d8baa205a0ee46794041013fdd6de7c
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-22 00:12:20 -0400

    refactor: Update HomeRoute component to use routes directory instead of components directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./App.scss";
 
-import HomeRoute from "components/HomeRoute";
+import HomeRoute from "routes/HomeRoute";
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   

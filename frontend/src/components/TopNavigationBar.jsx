d462eb8a3616281f4f5200892ea0841b95b10c76
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-21 23:52:26 -0400

    refactor: Update TopNavigationBar to use FavBadge instead of PhotoFavButton

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -8,7 +8,7 @@ const TopNavigation = () => {
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList />
-      <span><FavBadge/></span>
+      <FavBadge/>
     </div>
   )
 }

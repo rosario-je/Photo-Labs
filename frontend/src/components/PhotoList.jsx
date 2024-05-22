b752df69da885ab1a4890fedd00dfc89b3ddd2b8
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-22 15:14:28 -0400

    refactor: Update PhotoList component to include favourites and toggle functionality

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -5,9 +5,15 @@ import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
 const PhotoList = (props) => {
-  const { photos } = props;
+  const { photos, favourites, onToggleFavourite } = props;
+
   const allPhotos = photos.map((photo) => (
-    <PhotoListItem key={photo.id} data={photo} />
+    <PhotoListItem
+      key={photo.id}
+      data={photo}
+      favourites={favourites}
+      onToggleFavourite={onToggleFavourite}
+    />
   ));
   return <ul className="photo-list">{allPhotos}</ul>;
 };

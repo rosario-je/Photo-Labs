75fd2558fd5626c52346452ff5a80efce64412a7
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-22 12:35:17 -0400

    refactor: Update TopicList component to use props for topics data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,18 +1,15 @@
 import React from "react";
+
 import TopicListItem from "./TopicListItem";
+
 import "../styles/TopicList.scss";
-import topics from "mocks/topics";
 
-
-const TopicList = () => {
-  const allTopics = topics.map((topic) => 
+const TopicList = (props) => {
+  const { topics } = props;
+  const allTopics = topics.map((topic) => (
     <TopicListItem key={topic.id} topic={topic} />
-  )
-  return (
-    <div className="top-nav-bar__topic-list">
-      {allTopics}
-    </div>
-  );
+  ));
+  return <div className="top-nav-bar__topic-list">{allTopics}</div>;
 };
 
 export default TopicList;

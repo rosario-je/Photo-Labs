c4d79530b7c8ee9b8ad11cb20a23c15ab0932cb7
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-22 12:35:00 -0400

    refactor: Update HomeRoute component to pass topics and photos data to TopNavigation and PhotoList

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
 import PhotoList from "components/PhotoList";
 import TopNavigation from "components/TopNavigationBar";
 
-import '../styles/HomeRoute.scss';
+import "../styles/HomeRoute.scss";
 
-const HomeRoute = () => {
+const HomeRoute = (props) => {
+  const { topics, photos } = props;
   return (
     <div className="home-route">
-      <TopNavigation />
-      <PhotoList />
+      <TopNavigation topics={topics} />
+      <PhotoList photos={photos} />
     </div>
   );
 };

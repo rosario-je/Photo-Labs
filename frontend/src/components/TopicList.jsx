1846a0dea1a98c9dd9486e6e85958ae9dbf402fa
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-22 12:16:47 -0400

    refactor: Update PhotoList and TopicList components to use mock data from separate files

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,27 +1,11 @@
 import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
+import topics from "mocks/topics";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
 const TopicList = () => {
-  const allTopics = sampleDataForTopicList.map((topic) => 
+  const allTopics = topics.map((topic) => 
     <TopicListItem key={topic.id} topic={topic} />
   )
   return (

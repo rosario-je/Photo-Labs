c1341db764005ab8aaa87aa5f2f86ae25b2729d6
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-21 22:31:33 -0400

    refactor: Add TopicList component to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import PhotoList from "components/PhotoList";
 import "./App.scss";
+import TopicList from "components/TopicList";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <TopicList />
       <PhotoList />
     </div>
   );

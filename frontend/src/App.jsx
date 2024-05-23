14bd8fa958837e97ccc363aa1f0c0b365c240d4b
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-22 21:55:31 -0400

    refactor: Add modal window functionality to HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import HomeRoute from "routes/HomeRoute";
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
 import photos from "mocks/photos";
 import topics from "mocks/topics";
 
 import "./App.scss";
 
-
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function displayModalWindow() {
+    setModalVisible(modalVisible ? false : true);
+    console.log('article clicked');
+  }
 
   return (
     <div className="App">
-        <HomeRoute photos={photos} topics={topics}/>
+      <HomeRoute
+        photos={photos}
+        topics={topics}
+        displayModalWindow={displayModalWindow}
+      />
+      {modalVisible && <PhotoDetailsModal />}
     </div>
   );
 };

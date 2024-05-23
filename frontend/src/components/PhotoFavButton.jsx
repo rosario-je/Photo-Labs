f2cd1f324bd10f47ac15fc7f579290a0d515de21
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-22 21:08:18 -0400

    refactor: Update PhotoFavButton component to use single quotes consistently and fix formatting

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,14 +1,13 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import FavIcon from './FavIcon';
-import '../styles/PhotoFavButton.scss';
+import FavIcon from "./FavIcon";
+import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton(props) {
-
   return (
     <div className="photo-list__fav-icon">
       <div onClick={props.onClick} className="photo-list__fav-icon-svg">
-        <FavIcon selected={props.selected} displayAlert={props.displayAlert}/>
+        <FavIcon selected={props.selected} displayAlert={props.displayAlert} />
       </div>
     </div>
   );

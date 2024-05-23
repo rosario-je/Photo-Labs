6bcede644ea991d03470643503921d21534a3e87
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-23 00:10:40 -0400

    refactor: Destructure props for better readability and rename property names

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,11 +4,14 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
+  const {onClose, photosObject} = props
+  console.log(photosObject);
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={props.closeModalWindow}>
+      <button className="photo-details-modal__close-button" onClick={onClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      <img src={photosObject.urls.regular} alt="" />
     </div>
   )
 };

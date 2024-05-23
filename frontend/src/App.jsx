87bc1ed8aa6d0deced605391e39396342523beae
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-23 13:40:17 -0400

    refactor: Add favourites functionality to App component and PhotoDetailsModal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,15 @@ import "./App.scss";
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   const [toggleMondalWindow, setToggleModalWindow] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState(null)
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [favourites, setFavourites] = useState([]);
+  // console.log('this is the app component:', favourites);
+
+  const toggleFavourite = (photoId) => {
+    favourites.includes(photoId)
+      ? setFavourites(favourites.filter((id) => id !== photoId))
+      : setFavourites([...favourites, photoId]);
+  };
 
   const displayModalWindow = (data) => {
     setSelectedPhoto(data);
@@ -21,8 +29,6 @@ const App = () => {
     setToggleModalWindow(false);
     setSelectedPhoto(null);
   };
-  
-
 
   return (
     <div className="App">
@@ -30,10 +36,17 @@ const App = () => {
         photos={photos}
         topics={topics}
         displayModalWindow={displayModalWindow}
+        favourites={favourites}
+        toggleFavourite={toggleFavourite}
       />
-      {toggleMondalWindow && <PhotoDetailsModal onClose={closeModalWindow}
-        photo={selectedPhoto}
-      />}
+      {toggleMondalWindow && (
+        <PhotoDetailsModal
+          onClose={closeModalWindow}
+          photo={selectedPhoto}
+          favourites={favourites}
+          toggleFavourite={toggleFavourite}
+        />
+      )}
     </div>
   );
 };

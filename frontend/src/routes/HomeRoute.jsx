056b98770c8d91858f11f7e18c8de0519569edca
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-23 13:45:16 -0400

    refactor: Update HomeRoute component to destructure props for better readability and consistency

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -6,26 +6,17 @@ import TopNavigation from "components/TopNavigationBar";
 import "../styles/HomeRoute.scss";
 
 const HomeRoute = (props) => {
-  const { topics, photos, displayModalWindow } = props;
-  
-  //Set favorite state
-  const [favourites, setFavourites] = useState([]);
-
-  const toggleFavourite = (photoId) => {
-    favourites.includes(photoId)
-      ? setFavourites(favourites.filter((id) => id !== photoId))
-      : setFavourites([...favourites, photoId]);
-  };
+  const { topics, photos, displayModalWindow, toggleFavourite, favourites } =
+    props;
 
   return (
     <div className="home-route">
-      <TopNavigation topics={topics} favourites={favourites}/>
+      <TopNavigation topics={topics} favourites={favourites} />
       <PhotoList
         photos={photos}
         favourites={favourites}
         onToggleFavourite={toggleFavourite}
         displayModalWindow={displayModalWindow}
-        
       />
     </div>
   );

c08b35f2254cdcece264daa35c31ec162351374b
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-23 21:41:26 -0400

    refactor: Update TopNavigationBar component to include a wrapper div for topics and favourites

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -10,8 +10,10 @@ const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics} />
-      <FavBadge isFavPhotoExist={favourites} />
+      <div className="top-nav-bar__topics">
+        <TopicList topics={topics} />
+        <FavBadge isFavPhotoExist={favourites} />
+      </div>
     </div>
   );
 };

1e046ae5f11294b54405971647e4ce7bd0eefae5
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-24 23:05:20 -0400

    refactor: Update useApplicationData hook to useReducer for state management

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,31 +1,110 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
+
+function reducer(state, action) {
+  const updatedState = { ...state }
+  const { type, payload } = action
+  switch (type) {
+    case ACTIONS.FAV_PHOTO_ADDED:
+      // const photoId = action.payload.photoId
+      // const updatedFavourites = [...state.favourites, photoId]
+      // updatedState.favourites = updatedFavourites
+      // return updatedState
+      return { ...state, favourites: [...state.favourites, payload.photoId] }
+      
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      updatedState.favourites = state.favourites.filter((id) => id !== payload.photoId);
+      return updatedState;
+
+    case ACTIONS.SELECT_PHOTO:
+      // const updatedSelectedPhoto = payload.data
+      // updatedState.selectedPhoto = updatedSelectedPhoto;
+      // console.log("TEST");
+      updatedState.selectedPhoto = payload.data;
+      return updatedState
+
+
+    case ACTIONS.DISPLAY_PHOTO_DETAILS:
+      // const newModalState = payload.modalWindowState;
+      // updatedState.toggleModalWindow = newModalState;
+      // console.log("MODAL TEST");
+      updatedState.toggleModalWindow = payload.modalWindowState;
+      return updatedState;
+
+
+    case ACTIONS.SET_PHOTO_DATA:
+    //Logic to be implemented
+
+    case ACTIONS.SET_TOPIC_DATA:
+    //Logic to be implemented
+
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
 
 const useApplicationData = () => {
-  const [toggleMondalWindow, setToggleModalWindow] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const [favourites, setFavourites] = useState([]);
+  const [state, dispatch] = useReducer(reducer,
+    {
+      toggleModalWindow: false,
+      selectedPhoto: null,
+      favourites: []
+    })
 
+  /*--------------    Toggle Fav Photo   --------------*/
   const toggleFavourite = (photoId) => {
-    favourites.includes(photoId)
-      ? setFavourites(favourites.filter((id) => id !== photoId))
-      : setFavourites([...favourites, photoId]);
+
+    state.favourites.includes(photoId) ? dispatch({
+      type: ACTIONS.FAV_PHOTO_REMOVED,
+      payload: { photoId }
+    }) : dispatch({
+      type: ACTIONS.FAV_PHOTO_ADDED,
+      payload: { photoId }
+    })
   };
 
+  /*--------------    Display Modal Window   --------------*/
   const displayModalWindow = (data) => {
-    setSelectedPhoto(data);
-    setToggleModalWindow(true);
+    //Calls reducer function 
+    dispatch({
+      type: ACTIONS.SELECT_PHOTO,
+      payload: { data }
+    });
+    dispatch({
+      type: ACTIONS.DISPLAY_PHOTO_DETAILS,
+      payload: { modalWindowState: true }
+    })
   };
 
+  /*--------------    Close Modal Window   --------------*/
   const closeModalWindow = () => {
-    setToggleModalWindow(false);
-    setSelectedPhoto(null);
-  };
+    //Calls reducer function and sets the selected photo to null; 
+    dispatch({
+      type: ACTIONS.SELECT_PHOTO,
+      payload: { data: null }
+    });
+    //Calls reducer function and sets the toggleModalWindow state to false
+    dispatch({
+      type: ACTIONS.DISPLAY_PHOTO_DETAILS,
+      payload: { modalWindowState: false }
+    })
 
+  };
 
   return {
-    toggleMondalWindow,
-    selectedPhoto,
-    favourites,
+    toggleModalWindow: state.toggleModalWindow,
+    selectedPhoto: state.selectedPhoto,
+    favourites: state.favourites,
     toggleFavourite,
     displayModalWindow,
     closeModalWindow

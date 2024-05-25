defb9fd9cfec10aec8f5ae9046b52b50b6152f3e
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-25 14:23:28 -0400

    refactor: Update useApplicationData hook to remove useState import and useReducer for state management

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 
 export const ACTIONS = {
   FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
@@ -14,39 +14,22 @@ function reducer(state, action) {
   const { type, payload } = action
   switch (type) {
     case ACTIONS.FAV_PHOTO_ADDED:
-      // const photoId = action.payload.photoId
-      // const updatedFavourites = [...state.favourites, photoId]
-      // updatedState.favourites = updatedFavourites
-      // return updatedState
       return { ...state, favourites: [...state.favourites, payload.photoId] }
-      
+
     case ACTIONS.FAV_PHOTO_REMOVED:
-      // updatedState.favourites = state.favourites.filter((id) => id !== payload.photoId);
-      // return updatedState;
-      return { ...state, favourites: [...state.favourites.filter((id) => id !== payload.photoId)]};
+      return { ...state, favourites: [...state.favourites.filter((id) => id !== payload.photoId)] };
 
     case ACTIONS.SELECT_PHOTO:
-      // const updatedSelectedPhoto = payload.data
-      // updatedState.selectedPhoto = updatedSelectedPhoto;
-      // updatedState.selectedPhoto = payload.data;
-      // return updatedState
-      return {...state, selectedPhoto: payload.data}
-
+      return { ...state, selectedPhoto: payload.data }
 
     case ACTIONS.DISPLAY_PHOTO_DETAILS:
-      // const newModalState = payload.modalWindowState;
-      // updatedState.toggleModalWindow = newModalState;
-      // console.log("MODAL TEST");
-      // updatedState.toggleModalWindow = payload.modalWindowState;
-      // return updatedState;
-      return {...state, toggleModalWindow: payload.modalWindowState}
+      return { ...state, toggleModalWindow: payload.modalWindowState }
 
     case ACTIONS.SET_PHOTO_DATA:
     //Logic to be implemented
 
     case ACTIONS.SET_TOPIC_DATA:
     //Logic to be implemented
-
     default:
       throw new Error(
         `Tried to reduce with unsupported action type: ${action.type}`
@@ -64,40 +47,19 @@ const useApplicationData = () => {
 
   /*--------------    Toggle Fav Photo   --------------*/
   const toggleFavourite = (photoId) => {
-
-    state.favourites.includes(photoId) ? dispatch({
-      type: ACTIONS.FAV_PHOTO_REMOVED,
-      payload: { photoId }
-    }) : dispatch({
-      type: ACTIONS.FAV_PHOTO_ADDED,
-      payload: { photoId }
-    })
+    state.favourites.includes(photoId)
+      ? dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: { photoId } })
+      : dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: { photoId } })
   };
-
   /*--------------    Display Modal Window   --------------*/
   const displayModalWindow = (data) => {
-    //Calls reducer function 
-    dispatch({
-      type: ACTIONS.SELECT_PHOTO,
-      payload: { data }
-    });
-    dispatch({
-      type: ACTIONS.DISPLAY_PHOTO_DETAILS,
-      payload: { modalWindowState: true }
-    })
+    dispatch({ type: ACTIONS.SELECT_PHOTO, payload: { data } });
+    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS, payload: { modalWindowState: true } })
   };
-
   /*--------------    Close Modal Window   --------------*/
   const closeModalWindow = () => {
-    dispatch({
-      type: ACTIONS.SELECT_PHOTO,
-      payload: { data: null }
-    });
-    dispatch({
-      type: ACTIONS.DISPLAY_PHOTO_DETAILS,
-      payload: { modalWindowState: false }
-    })
-
+    dispatch({ type: ACTIONS.SELECT_PHOTO, payload: { data: null } });
+    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS, payload: { modalWindowState: false } })
   };
 
   return {

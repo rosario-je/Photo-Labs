125765665f05b8a05abd56cb6a817e40a21cd914
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-26 20:04:38 -0400

    refactor: Update FavBadge and FavIcon components to use consistent import statements and class names

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import FavIcon from './FavIcon';
+import React, { useState } from "react";
+import FavIcon from "./FavIcon";
 
-import '../styles/FavBadge.scss';
+import "../styles/FavBadge.scss";
 
 const FavBadge = ({ isFavPhotoExist }) => {
   const hasFavPhotos = isFavPhotoExist.length > 0;
-  
+
   return (
-    <div className='fav-badge'>
-      <FavIcon displayAlert={hasFavPhotos} selected={hasFavPhotos}/>
+    <div className="fav-badge">
+      <FavIcon displayAlert={hasFavPhotos} selected={hasFavPhotos} />
     </div>
-  ) 
+  );
 };
 
 export default FavBadge;

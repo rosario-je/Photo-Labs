f524f4e0953ca577903f8a468c90f3825195e724
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-27 19:09:26 -0400

    lint: Add comment explaning how the fav icon is changed

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -4,6 +4,7 @@ import FavIcon from "./FavIcon";
 import "../styles/FavBadge.scss";
 
 const FavBadge = ({ isFavPhotoExist }) => {
+  // If there is at least one favourited photo, trigger conditional rendering to show the appropriate icon by sending a truthy prop value to the FavIcon component
   const hasFavPhotos = isFavPhotoExist.length > 0;
 
   return (

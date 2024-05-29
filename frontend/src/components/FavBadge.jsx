3ef1384582242cbd6bf6e092491397f5de7137d7
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-05-29 13:12:55 -0400

    refactor: Add click event to FavBadge component to display favorite photos

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,13 +3,17 @@ import FavIcon from "./FavIcon";
 
 import "../styles/FavBadge.scss";
 
-const FavBadge = ({ isFavPhotoExist }) => {
+const FavBadge = ({ isFavPhotoExist, displayFavPhotos }) => {
   // If there is at least one favourited photo, trigger conditional rendering to show the appropriate icon by sending a truthy prop value to the FavIcon component
   const hasFavPhotos = isFavPhotoExist.length > 0;
 
+
   return (
-    <div className="fav-badge">
-      <FavIcon displayAlert={hasFavPhotos} selected={hasFavPhotos} />
+    <div className="fav-badge" onClick={() => displayFavPhotos()}>
+      <FavIcon
+        displayAlert={hasFavPhotos}
+        selected={hasFavPhotos}
+      />
     </div>
   );
 };
